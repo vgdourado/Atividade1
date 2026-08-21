d5d596818ce850208d2dff08256d3e89973c9d43
Vitória Gabriele <vitoriagdouradosouza008@gmail.com>
2026-08-20 23:04:27 -0400

    Add files via upload

---
+++ Atividades de Revisão/Atividades 1/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String nome = "Vitória";
+        int idade = "21";
+        double altura = "1.56";
+        double peso = "60.0";
+        boolean possuiCarteira = false;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade + " anos");
+        System.out.println("Altura: " + altura + " m");
+        System.out.println("Peso: " + peso + " kg");
+        System.out.println("Possui carteira de metorista: " + possuiCarteira);
+    }
+}
